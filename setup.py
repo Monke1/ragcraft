ed49342b4a7609b0ab4ce9bd820b740c4cb9369a
chu2bard <bxndnbdnd29@gmail.com>
2025-11-07 14:02:03 +0100

    patch schema

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="ragcraft",
-    version="0.1.0",
+    version="0.2.0",
     description="RAG pipeline with built-in evaluation",
     author="chu2bard",
     license="MIT",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=[
-# todo: revisit later
         "openai>=1.0",
         "numpy>=1.24",
         "faiss-cpu>=1.7",
